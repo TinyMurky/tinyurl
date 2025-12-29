bb8129a1ac03b59da5728af27fafaba26988f46c
TinyMurky <murky0830@gmail.com>
2025-12-29 13:43:31 +0800

    [chore] add cache config to url shortener

--- internal/urlshortener/config/config.go
+++ internal/urlshortener/config/config.go
@@ -1,12 +1,16 @@
 // Package urlshortenerconfig define the config of urlshortener
 package urlshortenerconfig
 
-import "github.com/TinyMurky/tinyurl/pkg/database"
+import (
+	"github.com/TinyMurky/tinyurl/pkg/cache"
+	"github.com/TinyMurky/tinyurl/pkg/database"
+)
 
 // Config for urlshortener
 // it need to use github.com/sethvargo/go-envconfig package to read
 type Config struct {
 	Database database.Config
+	Cache    cache.Config
 
 	IDGenerator    IDGeneratorConfig
 	Port           string `env:"PORT"`
@@ -16,4 +20,9 @@ type Config struct {
 // DatabaseConfig return Database config
 func (c *Config) DatabaseConfig() *database.Config {
 	return &c.Database
+}
+
+// CacheConfig return the config of cache
+func (c *Config) CacheConfig() *cache.Config {
+	return &c.Cache
 }
